d37915cd2286582d3deab5ac54ddaf2471943610
Rodrigo Roson <rodrigorosonsantana@gmail.com>
2026-02-23 17:24:26 +0100

    improved logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,19 +1,29 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from pathlib import Path
 
+# 1. Asegurar que la carpeta física existe
 Path("logs").mkdir(exist_ok=True)
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        logging.FileHandler("logs/pipeline.log"),
-        logging.StreamHandler()
-    ]
-)
-
 def get_logger(name: str) -> logging.Logger:
-    """
-    Devuelve un logger configurado para el módulo que lo solicita.
-    """
-    return logging.getLogger(name)
+    logger = logging.getLogger(name)
+
+    logger.propagate = False
+
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        file_handler = RotatingFileHandler(
+            "logs/pipeline.log",
+            maxBytes=5_242_880, # 5 MB
+            backupCount=3
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+    return logger
